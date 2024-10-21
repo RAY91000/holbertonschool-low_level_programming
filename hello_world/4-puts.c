f9b84a20cb84f501e4b0656d5383faa13ed6c65d
root <ray.dotropg@gmail.com>
2024-10-21 12:10:33 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,12 @@
-#!/bin/bash
-int main() {
-	puts(Write a C program that prints exactly "Programming is like building a multilingual puzzle")
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
