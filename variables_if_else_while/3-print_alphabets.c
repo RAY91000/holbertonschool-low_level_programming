185226aad04558b633cf2fa3cf0fde499e126b9d
ray91000 <ray.dotropg@gmail.com>
2024-10-22 18:08:54 +0200

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+
+/**
+ * main - entry point
+ *
+ * description: print the alphabet in lowercase, then in uppercase,
+ * followed by a new line
+ *
+ * Return: Always O (Sucess)
+ */
+int main(void)
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+
+		putchar(letter);
+	}
+
+	putchar('\n')
+
+	return (0);
