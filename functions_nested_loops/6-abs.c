afa159bc901fdecdd1f5cb38338baaef550895df
root <ray.dotropg@gmail.com>
2024-10-25 15:56:56 +0000

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -8,9 +8,9 @@
  */
 int _abs(int n)
 {
-    if (n < 0)
-    {
-        return -n; /* Return the negation of n if it's negative */
-    }
-    return n; /* Return n if it's non-negative */
+	if (n < 0)
+	{
+		return -n; /* Return the negation of n if it's negative */
+	}
+	return n; /* Return n if it's non-negative */
 }
