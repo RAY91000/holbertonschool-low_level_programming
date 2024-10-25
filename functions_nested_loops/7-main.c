05d3ee8d0f6eb839745e0624cbdf526422a8a768
root <ray.dotropg@gmail.com>
2024-10-25 17:12:17 +0000

    7

--- functions_nested_loops/7-main.c
+++ functions_nested_loops/7-main.c
@@ -10,10 +10,9 @@ int main(void)
     int r;
 
     r = print_last_digit(98);
-    _putchar('\n'); // Optional: print a newline after the first call
     r = print_last_digit(0);
-    _putchar('\n'); // Optional: print a newline after the second call
     r = print_last_digit(-1024);
+    _putchar('0' + r);
     _putchar('\n'); // Optional: print a newline after the third call
     return (0);
 }
