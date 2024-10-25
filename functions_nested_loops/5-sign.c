9916fe7182bf13d10c546f8a12a3c53dc1a42fb3
root <ray.dotropg@gmail.com>
2024-10-25 15:48:14 +0000

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Imprime le signe d'un nombre
+ * @n: Le nombre dont on veut connaître le signe
+ *
+ * Return: 1 et imprime + si n est supérieur à zéro,
+ *         0 et imprime 0 si n est zéro,
+ *         -1 et imprime - si n est inférieur à zéro
+ */
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
+}
