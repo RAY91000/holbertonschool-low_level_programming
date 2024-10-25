18c17281031919c97992e1bbfc2798563f42f68b
root <ray.dotropg@gmail.com>
2024-10-25 17:44:44 +0000

    8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,20 +7,20 @@
  */
 void jack_bauer(void)
 {
-    int hour;
-    int minute;
+int hour;
+int minute;
 
-    for (hour = 0; hour < 24; hour++)
-    {
-        for (minute = 0; minute < 60; minute++)
-        {
-            /* Afficher l'heure et les minutes */
-            _putchar(hour / 10 + '0');
-            _putchar(hour % 10 + '0');
-            _putchar(':');
-            _putchar(minute / 10 + '0');
-            _putchar(minute % 10 + '0');
-            _putchar('\n');
-        }
-    }
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+	/* Afficher l'heure et les minutes */
+_putchar(hour / 10 + '0');
+_putchar(hour % 10 + '0');
+_putchar(':');
+_putchar(minute / 10 + '0');
+_putchar(minute % 10 + '0');
+_putchar('\n');
+}
+}
 }
