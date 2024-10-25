1406b44e0fc68dececb9098a291635ceb05b82c0
root <ray.dotropg@gmail.com>
2024-10-25 15:38:10 +0000

    4

--- functions_nested_loops/4-main.c
+++ functions_nested_loops/4-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Test la fonction _islower
+ * main - Teste la fonction _isalpha
  *
  * Return: Toujours 0
  */
@@ -10,13 +10,18 @@ int main(void)
     int r;
 
     r = _isalpha('H');
-    _putchar(r + '0');
+    _putchar(r + '0'); /* Affiche '1' si 'H' est une lettre */
+
     r = _isalpha('o');
-    _putchar(r + '0');
-    r = _isalpha(108);
-    _putchar(r + '0);
-    r = _isalpha(';');
-    _putchar(r + '0');
+    _putchar(r + '0'); /* Affiche '1' si 'o' est une lettre */
+
+    r = _isalpha('1');
+    _putchar(r + '0'); /* Affiche '0' si '1' n'est pas une lettre */
+
+    r = _isalpha(';');  /* Remplacer ';' par un caractère valide, si besoin */
+    _putchar(r + '0'); /* Affiche le résultat pour ';' */
+
     _putchar('\n');
 
     return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _islower(int c);
-int _putchar(char c);  /* Prototype uniquement */
+int _putchar(char c);
+int _isalpha(int c);
 
 #endif /* MAIN_H */
