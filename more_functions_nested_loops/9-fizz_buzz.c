eff942c04fef771cc9a4f14e10d7147dce435da6
root <ray.dotropg@gmail.com>
2024-10-28 21:51:25 +0000

    9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -8,21 +8,23 @@
  */
 int main(void)
 {
-for (int i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else
-printf("%d", i);
+    int i;
 
-if (i < 100)
-printf(" ");
-}
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            printf("FizzBuzz");
+        else if (i % 3 == 0)
+            printf("Fizz");
+        else if (i % 5 == 0)
+            printf("Buzz");
+        else
+            printf("%d", i);
 
-printf("\n");
-return (0);
+        if (i < 100)
+            printf(" ");
+    }
+
+    printf("\n");
+    return (0);
 }
