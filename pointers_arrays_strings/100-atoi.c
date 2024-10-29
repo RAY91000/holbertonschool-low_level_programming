6136ccf4841f7ac334e3d48a5fa84263abb6b432
root <ray.dotropg@gmail.com>
2024-10-29 21:45:18 +0000

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,22 @@
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    unsigned int num = 0;
-    int started = 0;
+int sign = 1;
+unsigned int num = 0;
+int started = 0;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            num = num * 10 + (*s - '0');
-            started = 1;
-        }
-        else if (started)
-            break;
-        s++;
-    }
-    return (sign * num);
+while (*s)
+{
+if (*s == '-')
+sign *= -1;
+else if (*s >= '0' && *s <= '9')
+{
+num = num * 10 + (*s - '0');
+started = 1;
+}
+else if (started)
+break;
+s++;
+}
+return (sign * num);
 }
