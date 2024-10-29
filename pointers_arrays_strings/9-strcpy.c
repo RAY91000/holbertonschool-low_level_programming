1984581dbfa6fd69983d40cc7b77bcc55ae594fa
root <ray.dotropg@gmail.com>
2024-10-29 21:33:50 +0000

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string pointed to by src to dest.
+ * @dest: The destination buffer.
+ * @src: The source string to be copied.
+ *
+ * Return: A pointer to the destination string (dest).
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+
+return dest;
+}
