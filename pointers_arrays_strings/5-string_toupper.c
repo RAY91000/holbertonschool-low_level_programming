48e25981fff09d331f637fca598080e3c20e9ac0
root <ray.dotropg@gmail.com>
2024-10-30 21:18:57 +0000

    5.2

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,15 +8,15 @@
  */
 char *string_toupper(char *s)
 {
-    char *original = s;
+char *original = s;
 
-    while (*s)
-    {
-        if (*s >= 'a' && *s <= 'z')
-        {
-            *s -= 32;
-        }
-        s++;
-    }
+while (*s)
+{
+if (*s >= 'a' && *s <= 'z')
+{
+*s -= 32;
+}
+s++;
+}
 return (original);
 }
