5401010d021285c9ea4d101bec735a1c10462e0d
root <ray.dotropg@gmail.com>
2024-10-30 21:17:19 +0000

    5.2

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @s: The string to be converted.
+ *
+ * Return: A pointer to the resulting string.
+ */
+char *string_toupper(char *s)
+{
+    char *original = s;
+
+    while (*s)
+    {
+        if (*s >= 'a' && *s <= 'z')
+        {
+            *s -= 32;
+        }
+        s++;
+    }
+return (original);
+}
