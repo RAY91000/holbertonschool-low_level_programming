0f0886db94f4bbd91443bfad40d56aa73f25dc99
root <ray.dotropg@gmail.com>
2024-11-05 00:33:18 +0100

    8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void print_diagsums(int *a, int size)
+{
+int sum1 = 0, sum2 = 0;
+int i;
+
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * (size + 1)];
+}
+for (i = 0; i < size; i++)
+{
+sum2 += a[(i + 1) * (size - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
+}
