a9baa8c91943d9212acd71ffb8ca1c130a27eef5
root <ray.dotropg@gmail.com>
2024-11-04 12:27:03 +0100

    0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 
 {
-unsigned int i;
-for (i = 0; i < n; i++);
-{
-s[i] = b;
-}
-return (s);
+
+	unsigned int i;
+	for (i = 0; i < n; i++)
+		{
+			s[i] = b;
+		}
+
+	return (s);
+
 }
