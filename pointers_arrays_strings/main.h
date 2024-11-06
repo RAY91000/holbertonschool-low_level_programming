5cdd3114931d8f1eff94e7351119b539016998e7
root <ray.dotropg@gmail.com>
2024-11-06 17:22:06 +0100

    main.h update

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,6 +3,12 @@
 
 int _putchar(char c);
 char *_strpbrk(char *s, char *accept);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_memset(char *s, char b, unsigned int n);
 char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
