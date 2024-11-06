8143e2bcfba509b4363f5f96a1fcce0edae5fc00
root <ray.dotropg@gmail.com>
2024-11-06 19:08:51 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -2,9 +2,9 @@
 
 /**
  * is_prime_number - Vérifie si un nombre est premier
- * @n: Le nombre à tester
+ * @n: Le nombre à vérifier
  *
- * Return: 1 si n est un nombre premier, sinon 0
+ * Return: 1 si le nombre est premier, 0 sinon
  */
 int is_prime_number(int n)
 
