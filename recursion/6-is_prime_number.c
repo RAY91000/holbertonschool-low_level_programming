b390b34f0132ed5a26108711481227dbd9b3422e
root <ray.dotropg@gmail.com>
2024-11-06 20:06:06 +0000

    6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,18 +7,28 @@
  * Return: 1 si le nombre est premier, 0 sinon
  */
 int is_prime_number(int n)
-
 {
-	int i;
+    if (n <= 1)  /* Les nombres <= 1 ne sont pas premiers */
+        return (0);
 
-	if (n <= 1)
-		return (0);
-
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-			return (0);
-	}
-
-	return (1);
+    return (check_prime(n, 2));  /* Appel à la fonction récursive de vérification */
 }
+
+/**
+ * check_prime - Fonction récursive pour vérifier la primalité
+ * @n: Le nombre à vérifier
+ * @i: Le diviseur potentiel
+ *
+ * Return: 1 si n est premier, 0 sinon
+ */
+int check_prime(int n, int i)
+{
+    if (i * i > n)  /* Si i^2 > n, cela signifie que n n'a pas de diviseur inférieur à sqrt(n) */
+        return (1);
+
+    if (n % i == 0)  /* Si n est divisible par i, ce n'est pas un nombre premier */
+        return (0);
+
+    return (check_prime(n, i + 1));  /* Vérifie avec le prochain diviseur possible */
+}
+
