f9bfbbf10048afe0b23853580028539b98eb9f37
root <ray.dotropg@gmail.com>
2024-11-06 19:25:17 +0000

    5

--- recursion/main.h
+++ recursion/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stddef.h>
+
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
