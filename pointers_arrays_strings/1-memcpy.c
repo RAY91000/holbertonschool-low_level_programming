7985a524f4162b7ab6de0c6bb7acc1a2b5b57fcc
root <ray.dotropg@gmail.com>
2024-11-06 17:37:58 +0100

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
+/**
+ * _memcpy - Copie une zone de mémoire
+ * @dest: pointeur la destination où les data seront copié
+ * @src: pointeur vers la source des données à copier
+ * @n: nombre de bytes à copier de src vers dest
+ *
+ * Cette fonction copie n bytes de la zone mémoire pointée par src
+ * dans la zone mémoire pointée par dest.
+ *
+ * Return: Un pointeur vers la zone mémoire de destination (dest).
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-dest[i] = src[i];
-}
-return (dest);
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
