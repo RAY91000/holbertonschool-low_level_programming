8851559f33b8ca1e1920dc3f47a4008fdb044bbc
root <ray.dotropg@gmail.com>
2024-11-06 17:07:33 +0000

    0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Imprime une chaîne de caractères suivie d'un reto a la ligne
+ * @s: Pointeur vers la chaîne de caractèr a imprimer
+ *
+ * Description: Cette fonction utilise la récursivité pour imprimer chaque
+ * caractère de la chaîne, puis ajoute un retoua la ligne après la fin
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
