cd5b648f9e548b3aae094e0ac2864bb763b9c2e2
root <ray.dotropg@gmail.com>
2024-11-08 20:51:28 +0000

    maj8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * print_diagsums - Affiche la somme des deux diagonales d'une matrice carre
+ * @a: Pointeur vers la matrice carre d'entiers
+ * @size: Taille de la matrice (nombre de lignes et colonnes)
+ *
+ * Description: La fonction calcule la somme de la diagonale principale
+ * et de la diagonale secondaire d'une matrice carre de taille specifiee.
+ */
 
 void print_diagsums(int *a, int size)
 {
