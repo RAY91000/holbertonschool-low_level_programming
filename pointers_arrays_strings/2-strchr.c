1cacff987677d18f1ec3dab51ce4f3bfab9ac976
root <ray.dotropg@gmail.com>
2024-11-08 20:00:13 +0000

    maj2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
+/**
+ * _strchr - Localise un caractère dans une chaîne de caractères
+ * @s: La chaîne de caractèrea analyser
+ * @c: Le caractèr a rechercher dans la chaîne
+ *
+ * Return: Un pointeur vers la première occurrence du caractère c dans s,
+ * ou NULL si le caractère n'est pas trouvé
+ */
+
 char *_strchr(char *s, char c)
 
 {
-while (*s != '\0')
-{
-if (*s == c)
-{	
-return (s);
-}
-s++;
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 
-}
-if (c == '\0')
-{
-return (s);
-}
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
 
-return ('\0');
+	return ('\0');
 }
