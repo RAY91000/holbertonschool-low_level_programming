e779e1c62e1b1c92d5709f5093516cc3f264fbf3
root <ray.dotropg@gmail.com>
2024-11-08 20:17:38 +0000

    maj7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,17 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strspn - Calcule la longueur du prefixe d'une chaine composee
+ *           uniquement de certains caracteres
+ * @s: La chaine principale a analyser
+ * @accept: La chaine contenant les caracteres acceptables
+ *
+ * Return: Le nombre de caracteres dans le segment initial de s
+ * qui sont presents dans accept
+ */
 
 void print_chessboard(char (*a)[8])
+
 {
 int i, j;
 for (i = 0; i < 8; i++)
