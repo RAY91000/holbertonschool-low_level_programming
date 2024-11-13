3c113b6b3696dd78399ec4e874fa567e5100ee37
root <ray.dotropg@gmail.com>
2024-11-13 16:35:41 +0000

    1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - Concatène s1 avec les n premiers caractères de s2
- * @s1: Première chaîne de caractères
- * @s2: Deuxième chaîne de caractères
- * @n: Nombre de caractères de s2a concaténe
+ * string_nconcat - Concatene deux chaines en allouant de la memoire
+ * @s1: Premiere chaine de caracteres
+ * @s2: Deuxieme chaine de caracteres
+ * @n: Nombre maximum de caracteres a prendre de s2
  *
- * Return: Un pointeur vers la chaîne concaténée, ou NULL si l'allocation échoue
+ * Return: Pointeur vers la nouvelle chaine concatenee, ou NULL en cas d'echec
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, len_concat, i, j;
-	char *concat_str;
+	unsigned int len1 = 0, len2 = 0, i, j;
+	char *new_str;
 
 	if (s1 == NULL)
 		s1 = "";
-
 	if (s2 == NULL)
 		s2 = "";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
 
 	if (n >= len2)
 		n = len2;
 
-	len_concat = len1 + n;
-
-	concat_str = malloc(len_concat + 1);
-	if (concat_str == NULL)
+	new_str = malloc(sizeof(char) * (len1 + n + 1));
+	if (new_str == NULL)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)
-		concat_str[i] = s1[i];
+		new_str[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-		concat_str[i + j] = s2[j];
+		new_str[i + j] = s2[j];
 
-	concat_str[i + j] = '\0';
+	new_str[i + j] = '\0';
 
-	return (concat_str);
+	return (new_str);
 }
