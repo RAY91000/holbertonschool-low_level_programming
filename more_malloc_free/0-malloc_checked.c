0f3e047fa637b9e7112eaebed85c33859b49848e
root <ray.dotropg@gmail.com>
2024-11-13 16:22:46 +0000

    0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Alloue de la memoire en utilisant malloc
+ * @b: Taille de la memoire a allouer en octets
+ *
+ * Return: Un pointeur vers la memoire allouee
+ * Termine le processus avec un statut de 98 si malloc echoue
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
