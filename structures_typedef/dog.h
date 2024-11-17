25432b1afd4fce50b85437b077743bb04b5a8129
root <ray.dotropg@gmail.com>
2024-11-17 14:28:17 +0000

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure pour représenter un chien
+ * @name: Nom du chien (type char *)
+ * @age: Âge du chien (type float)
+ * @owner: Propriétaire du chien (type char *)
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
