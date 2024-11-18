d62c1dec410fab83bd8c7cdaaff205b5d61b6964
root <ray.dotropg@gmail.com>
2024-11-18 14:18:49 +0000

    1

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,6 +4,7 @@
 /* Inclus les bibliothèques nécessaires */
 #include <stddef.h>
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
 int _puthar(char c);
 
