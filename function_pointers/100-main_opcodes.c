1caac4d8db77c9e6ac34c73aee8dceae576b12b8
root <ray.dotropg@gmail.com>
2024-11-22 22:31:58 +0000

    100

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "function_pointers"
+#include "function_pointers.h"
 
 /**
- * main - Imprime les opcodes de sa propre fonction main
+ * main - Imprime les opcodes de la fonction main elle-même
  * @argc: Nombre d'arguments passés au programme
  * @argv: Tableau des arguments
  * Return: 0 en cas de succès, 1 ou 2 en cas d'erreur
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	int num_bytes, i;
 	unsigned char *ptr;
 
-	if (argc != 2) /* Vérifie que le bon nombre d'arguments est fourni */
+	if (argc != 2) /* Vérifie si un seul argument est fourni */
 	{
 		printf("Error\n");
 		return (1);
@@ -26,16 +26,16 @@ int main(int argc, char *argv[])
 		return (2);
 	}
 
-	ptr = (unsigned char *)main; /* Pointeur vers l'adresse de main */
+	ptr = (unsigned char *)main; /* Obtenir l'adresse de la fonction main */
 
-	/* Imprime les opcodes en hexadécimal */
+	/* Boucle pour afficher les opcodes */
 	for (i = 0; i < num_bytes; i++)
 	{
 		printf("%02x", ptr[i]);
 		if (i != num_bytes - 1)
 			printf(" ");
 	}
-
 	printf("\n");
+
 	return (0);
 }
